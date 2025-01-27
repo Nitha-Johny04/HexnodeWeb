7376e2bc43ea0b2552cf9c50051a324cba3067d5
Nitha-Johny04 <nithajohny1998@gmail.com>
2025-01-27 12:00:48 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import "./Header.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Awards from "./Awards";
 import KisokModes from "./KisokModes";
@@ -14,20 +14,32 @@ function Header() {
 
   const handleSubmit = () => {
     if (!email) {
-      setError('Please enter your work email!');
-      // Remove the error message after 2 seconds
+      setError("Please enter your work email!");
       setTimeout(() => {
-        setError('');
+        setError("");
       }, 2000);
     } else {
-      setError('');
+      setError("");
       // Handle the form submission (e.g., API call)
     }
   };
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
       <div className="landing-container">
-        <header className="header">
+        <header className={`header ${isScrolled ? "scrolled" : ""}`}>
           <div className="logo">hexnode</div>
           <button className="trial-button">14 DAY FREE TRIAL</button>
         </header>
@@ -35,9 +47,6 @@ function Header() {
           <div className="text-content">
             <h1 class="headline">
               Turn your devices into kiosks in a few minutes with Hexnode UEM
-              {/* Turn your devices into <br></br>
-              kiosks in a few minutes <br></br>
-              with Hexnode UEM */}
             </h1>
             <div className="email-form">
               <input
@@ -50,13 +59,10 @@ function Header() {
                 GET STARTED NOW!
               </button>
               {error && <p className="error-message">{error}</p>}
-              {/* <input type="email" placeholder="Your Work Email" />
-              <button className="cta-button">GET STARTED NOW!</button> */}
             </div>
           </div>
           <div className="image-content">
             <img src={Hexnode} alt="Tablet 1" className="tablet-image" />
-            {/* <img src={tablet2} alt="Tablet 2" className="tablet-image" /> */}
           </div>
         </div>
         <Awards></Awards>
